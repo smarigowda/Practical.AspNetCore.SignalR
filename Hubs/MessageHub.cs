546462d8caba9426186666183c6f8486dcc45c75
Santosh Marigowda <santosharakere@gmail.com>
2019-01-20 17:59:37 +0000

    sending message to specific connection

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -13,6 +14,20 @@ namespace Practical.AspNetCore.SignalR
         public Task SendMessageToCaller(string message)
         {
             return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
--- wwwroot/js/message.js
+++ wwwroot/js/message.js
@@ -10,20 +10,44 @@ connection.on('ReceiveMessage', function(message) {
     document.getElementById('messages').appendChild(div);
 });
 
+// Any client who is already connected then its connectedId will not be in the list
+// Only new connections will be in the list.
+connection.on('UserConnected', connectionId => {
+    const groupElement = document.getElementById('group');
+    const option = document.createElement('option')
+    option.text = connectionId;
+    option.value = connectionId;
+    groupElement.add(option);
+});
+
+connection.on('UserDisconnected', connectionId => {
+    const groupElement = document.getElementById('group');
+    for(let i = 0; groupElement.length; i++) {
+        if (groupElement.options[i].value === connectionId) {
+            groupElement.remove(i);
+        }
+    }
+});
+
 connection.start().catch(error => {
     console.error(error);
-})
+});
 
 document.getElementById('sendButton').addEventListener('click', function(event) {
     const groupElement = document.getElementById('group');
     const groupValue = groupElement.options[groupElement.selectedIndex].value;
-    let method = 'SendMessageToAll';
-    if(groupValue === 'Myself') {
-        method = 'SendMessageToCaller';
-    }
     const message = document.getElementById('message').value;
-    connection.invoke(method, message).catch(error => {
-        console.error(error);
-    });
+
+    if(groupValue === 'All' || groupValue === 'Myself') {
+        const method = groupValue === 'All'? 'SendMessageToAll' : 'SendMessageToCaller';
+        connection.invoke(method, message).catch(error => {
+            console.error(error);
+        });
+    } else {
+        connection.invoke('SendMessageToUser',  groupValue, message).catch(error => {
+            console.error(error);
+        });
+    }
+
     event.preventDefault();
-})
+});
