6ecc2a74ca57b1ea9dfa11253a5ce3eac3f5690d
Santosh Marigowda <santosharakere@gmail.com>
2019-01-20 14:23:25 +0000

    configuring SignalR

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,8 @@ namespace Practical.AspNetCore.SignalR
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,12 @@ namespace Practical.AspNetCore.SignalR
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseSignalR(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapHub<MessageHub>("/messages");
             });
+            app.UseMvc();
         }
     }
 }
