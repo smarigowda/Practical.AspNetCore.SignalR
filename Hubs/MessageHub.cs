d2789629d6866d3b471d7e4df0f2427fcfd09120
Santosh Marigowda <santosharakere@gmail.com>
2019-01-20 14:13:47 +0000

    initial commit

---
+++ Hubs/MessageHub.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Practical.AspNetCore.SignalR
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
